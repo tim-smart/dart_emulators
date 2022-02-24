af2da6fa927263275f7892a0a163e3e747e87872
Tim Smart <tim.smart@arisechurch.com>
2022-02-24 13:54:00 +1300

    export more classes

--- lib/emulators.dart
+++ lib/emulators.dart
@@ -3,3 +3,6 @@ library emulators;
 export 'src/device.dart' show Device, DeviceError, DeviceState, DevicePlatform;
 export 'src/emulators.dart';
 export 'src/environment.dart';
+export 'src/screenshot_helper.dart';
+export 'src/toolchain.dart';
+export 'src/utils/process.dart' show ProcessRunner;
