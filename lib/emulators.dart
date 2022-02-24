1a1e7b1f0c1fe13e7e3640b8dab2509866050859
Tim Smart <tim.smart@arisechurch.com>
2022-02-24 16:54:31 +1300

    Clean up exports some more

--- lib/emulators.dart
+++ lib/emulators.dart
@@ -1,8 +1,8 @@
 library emulators;
 
-export 'src/device.dart';
+export 'src/device.dart' show Device, DeviceError, DeviceState, DevicePlatform;
 export 'src/emulators.dart';
 export 'src/environment.dart';
 export 'src/screenshot_helper.dart';
-export 'src/toolchain.dart';
+export 'src/toolchain.dart' show Toolchain;
 export 'src/utils/process.dart' show ProcessRunner;
